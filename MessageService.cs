ababe5fc46ef5523ced56b29245d1281fceb9a38
eapetrovsky <eapetrovsky@users.noreply.github.com>
2020-02-21 08:48:14 +0200

    Initial commit

---
+++ MessageService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Forms;
+
+
+namespace TextEditor
+{
+    public interface IMessageService
+    {
+        void ShowMessage(string message);
+        void ShowExclamation(string exclamation);
+        void ShowError(string error);
+    }
+
+
+    class MessageService: IMessageService
+    {
+        public void ShowMessage(string message)
+        {
+            MessageBox.Show(message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public void ShowExclamation(string exclamation)
+        {
+            MessageBox.Show(exclamation, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        public void ShowError(string error)
+        {
+            MessageBox.Show(error, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+    }
+}
